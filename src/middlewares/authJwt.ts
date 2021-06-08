898dd3a8a4259767f2370ae8d6b3ca6c15ff6f93
Antonio <granviamurcia2@gmail.com>
2021-06-08 11:09:54 +0200

    Middleware to control jwt token on headers.

---
+++ src/middlewares/authJwt.ts
@@ -0,0 +1,25 @@
+import { Handler, NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { UserModel } from 'src/database/models/User'
+import { jwtDecodedData } from 'src/utils/types'
+import { env } from '..'
+
+export const verifyToken: Handler = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization
+  if (token === undefined) {
+    return res.status(403).json({ message: 'No token provided' })
+  }
+
+  if (typeof token !== 'string') {
+    return res.status(403).json({ message: 'The token provided is not a string' })
+  }
+
+  try {
+    const decoded = jwt.verify(token.substring(7), env.KEY) as jwtDecodedData
+    const user = await UserModel.findById(decoded.id, { password: 0 }).lean()
+    if (user == null) return res.status(404).json({ message: 'No user found' })
+    next()
+  } catch (e) {
+    return res.status(401).json({ message: 'Unauthorized!' })
+  }
+}
