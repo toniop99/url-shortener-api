649600e13055d1a088a4089fd45d30553f515ff3
Antonio <granviamurcia2@gmail.com>
2021-06-08 21:12:44 +0200

    Added Swagger

---
+++ src/swaggerOptions.ts
@@ -0,0 +1,28 @@
+import { env } from './index'
+import path from 'path'
+import { SwaggerOptions } from 'swagger-ui-express'
+
+export const getSwaggerOptions = (): SwaggerOptions => {
+  return {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Url shortener API',
+        version: '0.1.0',
+        description:
+          'An api for short urls',
+        contact: {
+          name: 'Antonio',
+          url: 'https://antoniohnz.me',
+          email: 'granviamurcia2@gmail.com'
+        }
+      },
+      servers: [
+        {
+          url: path.join(env.BASE_URL, 'v1/apì')
+        }
+      ]
+    },
+    apis: ['./docs/**/*.yaml']
+  }
+}
